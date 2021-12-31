5ff068399823d594267ce0128aa68442796f7865
sava <savamedvedev@gmail.com>
2021-12-31 13:03:17 +0800

    first-commit

---
+++ src/components/BasketItem.jsx
@@ -0,0 +1,40 @@
+function BasketItem(props) {
+  const {
+    id,
+    name,
+    price,
+    quantity,
+    removeFromBasket = Function.prototype,
+    decQuantity = Function.prototype,
+    incQuantity = Function.prototype,
+  } = props;
+
+  return (
+    <li className="collection-item">
+      {name}{" "}
+      <i
+        className="material-icons basket-quantity"
+        onClick={() => decQuantity(id)}
+      >
+        remove
+      </i>{" "}
+      x{quantity}{" "}
+      <i
+        className="material-icons basket-quantity"
+        onClick={() => incQuantity(id)}
+      >
+        add
+      </i>{" "}
+      = {price * quantity} руб.
+      <span
+        href="#!"
+        class="secondary-content"
+        onClick={() => removeFromBasket(id)}
+      >
+        <i class="material-icons basket-delete">close</i>
+      </span>
+    </li>
+  );
+}
+
+export { BasketItem };
